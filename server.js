e26e785ccdacc51214228acde058267746e5b3d6
Patrick <pjpweaver@gmail.com>
2015-12-12 14:50:09 -0500

    rename app to server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.send('Meredith is on vacation.');
+});
+
+app.listen(port, function(){
+  console.log('Running on PORT: ' + port);
+})
