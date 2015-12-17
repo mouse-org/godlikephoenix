5cba14fa29878fbc815d8fe6d1edc930e416538e
Patrick <pjpweaver@gmail.com>
2015-12-16 23:40:29 -0500

    set up api

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
-var express = require('express');
+var express = require('express'),
+  mongoose = require('mongoose');
 
+
+var db = mongoose.connect('mongodb://localhost/msvacation');
+
+var Vacation = require('./models/vacationModel');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var vacationRouter = express.Router();
+
+vacationRouter.route('/vacations')
+  .get(function(req,res){
+
+    var query = {};
+    if(req.query.place)
+      {
+        query.place = req.query.place;
+      }
+    Vacation.find(query, function(err,vacations){
+      if(err)
+        res.status(500).send(err);
+      else
+        res.json(vacations);
+    });
+  });
+
+vacationRouter.route('/vacations/:vacationId')
+  .get(function(req,res){
+
+    Vacation.findById(req.params.vacationId, function(err,vacation){
+      if(err)
+        res.status(500).send(err);
+      else
+        res.json(vacation);
+    });
+  });
+
+app.use('/api', vacationRouter);
+
+
+
 
 app.get('/', function(req, res){
   res.send('<div style="font-size: 300px;">Meredith is on vacation.</div>');
 });
 
 app.listen(port, function(){
-  console.log('Running on PORT: ' + port);
+  console.log('Gulp is running on PORT: ' + port);
 })
