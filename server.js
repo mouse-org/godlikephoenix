f794d8614f9b473e2569f75e8c84284bcf1cfe17
patrickmouse <patrick@mouse.org>
2016-06-07 17:20:53 -0400

    oops add all files for emoticon prototype

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars')
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
@@ -11,24 +10,15 @@ var request = require('request')
 
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'handlebars');
-
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-
-
-app.get('/', function(req, res){
-    res.render('index', {
-      css: 'style'
-  });
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/templates/index.html');
 });
-
-app.use('/public', express.static(__dirname + '/public'));
 
 app.listen(port, function(){
   console.log('Gulp is running on PORT: ' + port);
